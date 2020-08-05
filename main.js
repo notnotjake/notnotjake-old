2648a01db11ffc79d4ce235f28aaad3ba770002f
jgooch97 <jgooch@snowman.design>
2020-08-05 01:22:16 -0400

    Archives WIP
    Somewhat working. Added JS for archival scopes. Some behaviors not there yet, some issues to work out.

---
+++ main.js
@@ -0,0 +1,11 @@
+function archivesScoping(scope) {
+	var x = document.getElementById(scope);
+	if (x.style.display === "none") {
+		x.style.display = "block";
+	} else {
+		x.style.display = "none";
+	}
+	// var y = "scope-select-" + scope;
+	// var y = document.getElementById(y);
+	// if (y.style.font-weight: "bold")
+}
