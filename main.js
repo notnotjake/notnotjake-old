c01b931db2bf4090bd642e630dff09a6698fe537
jgooch97 <jgooch@snowman.design>
2020-08-05 02:03:13 -0400

    Desktop Archive Working
    mobile still not good but desktop is working nicely

--- main.js
+++ main.js
@@ -1,11 +1,22 @@
 function archivesScoping(scope) {
 	var x = document.getElementById(scope);
-	if (x.style.display === "none") {
+	if (x.style.display != "block") {
 		x.style.display = "block";
 	} else {
 		x.style.display = "none";
 	}
-	// var y = "scope-select-" + scope;
-	// var y = document.getElementById(y);
-	// if (y.style.font-weight: "bold")
+	var y = "scope-select-" + scope;
+	var y = document.getElementById(y);
+	if (y.style.fontWeight === "bold") {
+		y.style.fontWeight = "normal";
+	} else {
+		y.style.fontWeight = "bold";
+	}
+	var z = "scope-triangle-" + scope;
+	var z = document.getElementById(z);
+	if (z.style.display == "none") {
+		z.style.display = "inline";
+	} else {
+		z.style.display = "none";
+	}
 }
