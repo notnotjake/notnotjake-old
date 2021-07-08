e57281c8dad28d06fc93d4664b29c414506b8043
notnotjake <jake@notnotjake.com>
2021-07-08 18:01:03 -0400

    unorganized

---
+++ posts/articles.js
@@ -0,0 +1,34 @@
+const postsList = [
+	{
+		title: "Mach 1",
+		subtitle: "The First Batch of Apple Silicon Macs",
+		url: "https://notnotjake.com/posts/2020/11-12-mach-1/",
+		pubDateString: "Nov 12, 2020",
+		pubDateYear: 2020,
+		pubDateMonth: 11,
+		pubDateDay: 12,
+	}
+]
+
+const petsData = [
+	  {
+		name: "Purrsloud",
+		species: "Cat",
+		favFoods: ["wet food", "dry food", "<strong>any</strong> food"],
+		birthYear: 2016,
+		photo: "https://learnwebcode.github.io/json-example/images/cat-2.jpg"
+	  },
+	  {
+		name: "Barksalot",
+		species: "Dog",
+		birthYear: 2008,
+		photo: "https://learnwebcode.github.io/json-example/images/dog-1.jpg"
+	  },
+	  {
+		name: "Meowsalot",
+		species: "Cat",
+		favFoods: ["tuna", "catnip", "celery"],
+		birthYear: 2012,
+		photo: "https://learnwebcode.github.io/json-example/images/cat-1.jpg"
+	  }
+	];
